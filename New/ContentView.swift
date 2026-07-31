3511593191cdcd106dbf841b23e14eb5662ac606
Abbishek-07 <abbishekr24@dsce.ac.in>
2026-07-31 12:26:51 +0530

    Initial Commit

---
+++ New/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New
+//
+//  Created by STUDENT_22 on 31/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New/NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApp.swift
+//  New
+//
+//  Created by STUDENT_22 on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
